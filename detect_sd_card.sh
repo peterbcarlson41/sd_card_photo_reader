d45b6e6bd7c3e23c1775b522876aa46c21134620
peterbcarlson41 <75817779+peterbcarlson41@users.noreply.github.com>
2023-11-12 21:17:13 -0500

    Files are now copied over with their original date

--- detect_sd_card.sh
+++ detect_sd_card.sh
@@ -9,6 +9,7 @@ fi
 SDCARD_NAME="${SDCARD_NAME:-DefaultSDCardName}"
 TARGET_DIR="${TARGET_DIR:-/default/target/directory}"
 PHOTO_SUBDIRECTORY="${PHOTO_SUBDIRECTORY:-DefaultPhotoSubdirectory}"
+LAST_PROCESSED_FILE="$TARGET_DIR/.last_processed_date.txt"
 
 # Check for SD Card
 check_sd_card() {
@@ -19,30 +20,48 @@ check_sd_card() {
     fi
 }
 
-# Copy files with year and month organization
+# Function to convert date to a numeric value for comparison (macOS version)
+convert_date_to_numeric() {
+    date -j -f "%Y%m%d%H%M%S" "$1" "+%Y%m%d%H%M%S"
+}
+
+# Function to copy files with year, month, and optional subdirectory organization
 copy_files_from_sd_subdirectory() {
     SD_CARD_PATH="/Volumes/$SDCARD_NAME"
     FOUND_SUBDIRECTORY=$(find "$SD_CARD_PATH" -type d -name "$PHOTO_SUBDIRECTORY" -print -quit)
+    LAST_PROCESSED_TIMESTAMP=$(cat "$LAST_PROCESSED_FILE" 2>/dev/null || echo "19700101000000")
 
     if [ -n "$FOUND_SUBDIRECTORY" ]; then
+        new_files_found=false
+        latest_timestamp=$LAST_PROCESSED_TIMESTAMP
+
         for file in "$FOUND_SUBDIRECTORY"/*; do
             if [ -f "$file" ]; then
-                # Use exiftool to get the creation date
-                year_month=$(exiftool -DateTimeOriginal -d "%Y:%B" "$file" | awk -F': ' '{print $2}')
-                year=$(echo $year_month | cut -d: -f1)
-                month=$(echo $year_month | cut -d: -f2)
+                file_timestamp=$(date -r "$file" "+%Y%m%d%H%M%S")
+                
+                if [ "$file_timestamp" -gt "$LAST_PROCESSED_TIMESTAMP" ]; then
+                    year=${file_timestamp:0:4}
+                    month_num=${file_timestamp:4:2}
+                    month=$(LC_TIME=en_US.UTF-8 date -j -f "%m" "$month_num" "+%B")
 
-                # Fallback to file modification date if exif data is not available
-                if [ -z "$year" ] || [ -z "$month" ]; then
-                    year=$(date -r "$file" "+%Y")
-                    month=$(date -r "$file" "+%B")
+                    target_directory="$TARGET_DIR/$year/$month"
+                    mkdir -p "$target_directory"
+
+                    cp -p "$file" "$target_directory/"
+
+                    [ "$file_timestamp" -gt "$latest_timestamp" ] && latest_timestamp=$file_timestamp
+                    new_files_found=true
                 fi
-
-                # Create directory structure and copy file
-                mkdir -p "$TARGET_DIR/$year/$month"
-                cp -nv "$file" "$TARGET_DIR/$year/$month/"
             fi
         done
+
+        echo "$latest_timestamp" > "$LAST_PROCESSED_FILE"
+
+        if [ "$new_files_found" = true ]; then
+            echo "Files copied to $TARGET_DIR/$year/$month."
+        else
+            echo "No new files to copy."
+        fi
     else
         echo "Subdirectory named $PHOTO_SUBDIRECTORY not found in $SD_CARD_PATH."
     fi
@@ -53,8 +72,7 @@ while true; do
     if check_sd_card; then
         echo "SD Card $SDCARD_NAME detected."
         copy_files_from_sd_subdirectory
-        echo "Files copied to $TARGET_DIR."
-
+        
         while check_sd_card; do
             sleep 5
         done
